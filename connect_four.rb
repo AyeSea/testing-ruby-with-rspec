d503ebd67f8bf355e08831b8a5dabcd847b1c7d5
Andrew Chung <andrew.chung159@gmail.com>
2015-03-05 17:32:14 -0500

    updated Board class and its examples in spec file

--- connect_four.rb
+++ connect_four.rb
@@ -5,17 +5,18 @@ class Player
 		@name = name
 		@token = token
 	end
-
 end
 
+
 class Slot
-	attr_reader :value
+	attr_accessor :value
 
 	def initialize(value = '*')
 		@value = value
 	end
 end
 	
+
 class Board
 	attr_reader :layout
 
@@ -24,20 +25,59 @@ class Board
 	end
 
 	def show
-		puts "-----------------------------"
+		puts "\n    |C1 |C2 |C3 |C4 |C5 |C6 |C7 |"
+		puts "---------------------------------"
 
-		layout.each do |row|
+		layout.each_with_index do |row, i|
+			print "|R#{i + 1} "
+
 			row.each { |slot| print "| #{slot.value} " }
+			
 			print "|\n"
-			puts "-----------------------------"
+			puts "---------------------------------"
 		end
-	end	
+	end
+
+	def get_slot
+		row = get_row
+		col = get_col
+		layout[row][col]
+	end
+
+	def update_slot(token)
+		get_slot.value = token
+	end
+
+	def slot_empty?
+		row = get_row
+		col = get_col
+		layout[row][col].value == "*" ? true : false
+	end
+
+	def valid_move?
+		row = get_row
+		col = get_col
+		
+		return true if row == 5
+		layout[row + 1][col].value == "*" ? false : true
+	end
 
 	private
 	def create_board
 		Array.new(6) { Array.new(7) { Slot.new } }
 	end
+
+	def get_row
+		puts "Select a row:"
+		gets.chomp.to_i - 1
+	end
+
+	def get_col
+		puts "Select a column:"
+		gets.chomp.to_i - 1
+	end
 end
+
 
 class Game
 	attr_reader :board, :player1, :player2
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -47,9 +47,42 @@ describe Board do
 		end
 	end
 
-	context "#get_slot" do
-		xit "points to slot at (x,y)" do
+	before :each do
+		allow(board).to receive(:gets).and_return("6\n", "4\n")
+	end
 
+	context "#get_slot" do
+		it "points to slot at [5, 3] (1st row, center)" do
+			expect(board.get_slot).to eq board.layout[5][3]
+		end
+	end
+
+	context "#update_slot" do
+		it "updates slot at [5, 3] (1st row, center) to \u25ce" do
+			expect(board.update_slot("\u25ce")).to eq board.layout[5][3] = "\u25ce"
+		end
+	end
+
+	context "#slot_empty?" do
+		it "returns true if slot at [5, 3] (1st row, center) has no token" do
+			expect(board.slot_empty?).to be true
+		end
+
+		it "returns false if slot at [5, 3] (1st row, center) already has token" do
+			board.update_slot("\u25ce")			
+			allow(board).to receive(:gets).and_return("6\n", "4\n")			
+			expect(board.slot_empty?).to be false
+		end
+	end
+
+	context "#valid_move?" do
+		it "returns true when 1st move is a slot in the bottom (6th) row" do
+			expect(board.valid_move?).to be true
+		end
+
+		it "returns false when 1st move is a slot in the 2nd-to-last (5th) row" do
+			allow(board).to receive(:gets).and_return("5\n", "4\n")
+			expect(board.valid_move?).to be false
 		end
 	end
 end
